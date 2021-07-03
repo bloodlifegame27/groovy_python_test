0ab1b8626e5ddfe5c46875aa2d13b125d26da4c1
Dmytro Boiko <bloodlifegame27@gmail.com>
2021-07-03 18:14:43 +0300

    created pipeline_python.groovy

--- pipeline_python.groovy
+++ pipeline_python.groovy
@@ -1,10 +1,15 @@
 pipeline {
-  agent any
-  stages {
-    stage('Stage 1') {
-      steps {
-        echo 'Hello world!'
-      }
+    agent any
+    stages {
+        stage('Setup') {
+            steps {
+                echo 'Hello world!'
+            }
+        }
+        stage('python') {
+            steps {
+                sh 'python --version'
+            }
+        }
     }
-  }
 }
